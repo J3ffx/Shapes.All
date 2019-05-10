18c3133e08b69ba425ab0b25ebcdf41097f4cc53
Jérôme Fanfan <jeromexfanfan@gmail.com>
2019-05-10 17:45:21 +0200

    Quick Commit

--- src/graphics/shapes/interpret/CNewCircle.java
+++ src/graphics/shapes/interpret/CNewCircle.java
@@ -8,7 +8,7 @@ import graphics.shapes.ui.ShapesController;
 public class CNewCircle extends Command {
 
 	public CNewCircle() {
-		super.setName("circle");
+		super.setName("cire");
 	}
 
 	@Override
